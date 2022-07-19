ee67158ecc9a2d8729a41b92118482ad31474f5d
amirxoder <amirxoder@gmail.com>
2022-07-19 10:37:14 +0430

    now products can add to cart and remove from that

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -1,10 +1,19 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContextProvider";
-import { shorten } from "../../helpers/functions";
 
-const Product = ({ productData: { title, image, price, id } }) => {
-  
+//heloper functions
+import { isInCart, quntityCounter, shorten } from "../../helpers/functions";
+
+//icon
+import { BsFillTrashFill } from "react-icons/bs";
+import { FaCartPlus } from "react-icons/fa";
+import { HiPlus, HiMinusSm } from "react-icons/hi";
+
+const Product = ({ productData }) => {
+  const { state, dispatch } = useContext(CartContext);
+  const { title, image, price, id } = productData;
+
   return (
     <div>
       <img src={image} style={{ width: "200px" }} alt="product-img" />
@@ -12,7 +21,39 @@ const Product = ({ productData: { title, image, price, id } }) => {
       <p>{price}$</p>
       <div>
         <Link to={`/products/${id}`}>Details</Link>
-        <button>add to cart</button>
+        {isInCart(state, id) ? (
+          <button
+            onClick={() =>
+              dispatch({ type: "INCREASE_ITEM", payload: productData })
+            }
+          >
+            <HiPlus />
+          </button>
+        ) : (
+          <button
+            onClick={() => dispatch({ type: "ADD_ITEM", payload: productData })}
+          >
+            <FaCartPlus />
+          </button>
+        )}
+        {quntityCounter(state, id) > 1 && (
+          <button
+            onClick={() =>
+              dispatch({ type: "DECREASE_ITEM", payload: productData })
+            }
+          >
+            <HiMinusSm />
+          </button>
+        )}
+        {quntityCounter(state, id) === 1 && (
+          <button
+            onClick={() =>
+              dispatch({ type: "REMOVE_ITEM", payload: productData })
+            }
+          >
+            <BsFillTrashFill />
+          </button>
+        )}
       </div>
     </div>
   );
